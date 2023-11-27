001209a9b2c1badcb118b7d916d7b8f7d55414ac
Bashar <basharsaeed200@gmail.com>
2023-11-27 11:24:46 +0000

    worked on getting and adding recipe

--- js/db.js
+++ js/db.js
@@ -1,3 +1,49 @@
+
+// sync browser db with firebase cloud after offline
+db.enablePersistence()
+.catch(err=>{
+    if(err.code == 'failed-precondition'){
+        // Probably multiple tabs opened
+        console.log('persistence failed');
+    }else if(err.code == 'unimplemented'){
+        // lack of browser support
+        console.log('persistence not available')
+    }
+});
+
 db.collection('recipes').onSnapshot(snapshot=>{
-    console.log(snapshot.docChanges());
+    // console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+        console.log(change.type);
+
+        if(change.type === 'added'){
+            renderRecipe(change.doc.data(), change.doc.id)
+        }
+
+        if(change.type === 'removed'){
+
+        }
+
+        if(change.type === 'modified'){
+
+        }
+    });
+})
+
+const form = document.querySelector('form');
+form.addEventListener('submit', evt=>{
+    evt.preventDefault();
+
+    const recipe = {
+        title : form.title.value,
+        ingredients : form.ingredients.value
+    }
+
+    db.collection('recipes').add(recipe)
+    .catch(err=>{
+        console.log(err);
+    })
+
+    form.title.value = '';
+    form.ingredients.value = '';
 })
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v1';
-const dynamicCacheName = 'site-dynamic-v2';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v4';
 
 const assets = [
   '/',
@@ -53,21 +53,24 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  // evt.respondWith(
-  //   caches.match(evt.request).then(cacheRes => {
-  //     return cacheRes || fetch(evt.request).then(fetchRes => {
-  //       return caches.open(dynamicCacheName).then(cache => {
-  //         cache.put(evt.request.url, fetchRes.clone());
-  //         // check cached items size
-  //         limitCacheSize(dynamicCacheName, 15);
-  //         return fetchRes;
-  //       })
-  //     });
-  //   }).catch(() => {
-  //     if(evt.request.url.indexOf('.html') > -1){
-  //       return caches.match('/pages/fallback.html');
-  //     } 
-  //   })
-  // );
+
+  if(evt.request.url.indexOf('firestore.googleapi.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        } 
+      })
+    );
+  }
+
 });
